9c3e8e18a27aac086169bd10c35fa05b3d6ad791
thiago maia <thiago.maia@live.com>
2022-04-09 20:20:19 -0300

    10 terminado

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission) => {
+          const { name, year, country, destination } = mission;
+          return (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />
+          );
+        })}
       </div>
     );
   }
